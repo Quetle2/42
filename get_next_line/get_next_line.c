43c3f108e9b6f9a51953d72bb8a9ac0dc23d9d9b
Miguel Alexandre Andrade Arada <miandrad@c1r16s5.42lisboa.com>
2022-11-15 13:14:51 +0000

    privado

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,17 +6,22 @@
 /*   By: miandrad <miandrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 14:43:50 by miandrad          #+#    #+#             */
-/*   Updated: 2022/11/15 12:48:48 by miandrad         ###   ########.fr       */
+/*   Updated: 2022/11/15 13:08:25 by miandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*ft_linhador(char *src, char *dest)
+{
+	
+}
+
 char	*get_next_line(int fd)
 {
 	char		*buff;
 	static char	*temp;
-	// char		*linha = NULL;
+	char		*linha;
 	int			read_ret;
 
 	if (fd < 0)
@@ -34,10 +39,10 @@ char	*get_next_line(int fd)
 		buff[BUFFER_SIZE + 1] = '\0';
 		temp = ft_strjoin(temp, buff);
 	}
+	linha = ft_linhador(temp, linha);
 
 
 
 
-
-	return (temp);
+	return (linha);
 }
