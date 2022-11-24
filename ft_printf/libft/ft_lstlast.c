488316a478e77b24a5de4cdd93b101272f19c47e
Miguel Alexandre Andrade Arada <miandrad@c1r16s5.42lisboa.com>
2022-11-24 15:18:26 +0000

    eginning

---
+++ ft_printf/libft/ft_lstlast.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: miandrad <miandrad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/04 13:19:19 by miandrad          #+#    #+#             */
+/*   Updated: 2022/11/04 13:54:43 by miandrad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list	*temp;
+
+	temp = lst;
+	if (temp == NULL)
+		return (0);
+	while (temp->next != NULL)
+		temp = temp->next;
+	return (temp);
+}
--- ft_printf/libftprintf.h
+++ ft_printf/libftprintf.h
@@ -1,32 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: miandrad <miandrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/14 14:54:26 by miandrad          #+#    #+#             */
-/*   Updated: 2022/11/22 12:38:51 by miandrad         ###   ########.fr       */
+/*   Created: 2022/11/24 12:54:53 by miandrad          #+#    #+#             */
+/*   Updated: 2022/11/24 14:09:10 by miandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
 
+# include "libft/libft.h"
 # include <unistd.h>
-# include <stdlib.h>
-# include <stdio.h>
+# include <stdarg.h>
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1
-# endif
-
-char	*get_next_line(int fd);
-char	*ft_strjoin(char const *str1, char const *str2);
-size_t	ft_strlen(const char *str, size_t n);
-char	*ft_strchr(const char *str, int c);
-char	*ft_substr(char const *str, unsigned int start, size_t len);
-void	*ft_calloc(size_t noob, size_t size);
+int	ft_printf(const char *str, ...);
 
 #endif
-
